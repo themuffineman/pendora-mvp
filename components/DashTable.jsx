d41e10d09bad334fc768fce353f78cb50029d8e5
Petrus <petrusheya@gmail.com>
2024-08-27 19:31:12 +0200

    Added no data to display

--- components/DashTable.jsx
+++ components/DashTable.jsx
@@ -6,9 +6,14 @@ import { ScrollArea } from "@/components/ui/scroll-area"
 function DashTable({leads}) {
   return (
     <div className="overflow-y-auto grid grid-cols-1 grid-flow-row gap-4 items-center w-full p-3 rounded-md border border-neutral-200 h-[22rem]">
-      {leads.map((lead)=>(
-        <Lead lead={lead}/>
-      ))}
+      {leads.length === 0 ?(
+          <p>No Data to display</p>
+        )
+        :
+        leads.map((lead)=>(
+          <Lead lead={lead}/>
+        ))     
+      }
     </div>
   );
 }
